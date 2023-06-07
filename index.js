8a5e03b556be3e0a0ea1c5796e8345c8c35d5114
Aaron Sala <salaaron2@gmail.com>
2023-06-07 23:47:56 +0300

    completed

--- index.js
+++ index.js
@@ -7,12 +7,31 @@ menu.addEventListener('click',() =>{
 
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-   
-    
 })
-window.onscroll = ()=>{
+
+let section=document.querySelectorAll('section');
+let navLinks=document.querySelectorAll('header .navbar a');
+
+
+
+window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    section.forEach(sec =>{
+        let top = window.scrollY;
+        let height= section.offsetHeight;
+        let offset = section.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+
+        if(top => offset && top < offset + height ){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header .navbar a[href*='+id+']').classList.add('active')
+            });
+        };
+    });
 }
 
 document.querySelector('#search-icon').addEventListener ('click',() =>{
